2a8a1e6b06fe22e294fc3798e9557a560cfa5a1a
Onurkisayol0 <92668572+Onurkisayol0@users.noreply.github.com>
2023-11-27 22:31:22 +0300

    Update enough.py

--- enough.py
+++ enough.py
@@ -16,7 +16,7 @@ while 1:
     print("""{}
           【Ø】【N】【U】【R】
     
-    Sms: {}           {}by {}@tingirifistik\n  
+    Sms: {}           {}by {}@onurkisayol0\n  
     """.format(Fore.LIGHTCYAN_EX, len(servisler_sms), Style.RESET_ALL, Fore.LIGHTRED_EX))
     try:
         menu = (input(Fore.LIGHTMAGENTA_EX + " 1- SMS Gönder\n\n 2- Çıkış\n\n" + Fore.LIGHTYELLOW_EX + " Seçim: "))
