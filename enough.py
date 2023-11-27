672c00de31a5aae480a4cd82fd4e1fb7de273af5
Onurkisayol0 <92668572+Onurkisayol0@users.noreply.github.com>
2023-11-27 22:29:26 +0300

    Update enough.py

--- enough.py
+++ enough.py
@@ -14,14 +14,7 @@ for attribute in dir(SendSms):
 while 1:
     system("cls||clear")
     print("""{}
-     ______                         _     
-    |  ____|                       | |    
-    | |__   _ __   ___  _   _  __ _| |__  
-    |  __| | '_ \ / _ \| | | |/ _` | '_ \ 
-    | |____| | | | (_) | |_| | (_| | | | |
-    |______|_| |_|\___/ \__,_|\__, |_| |_|
-                               __/ |      
-                              |___/      
+          【Ø】【N】【U】【R】
     
     Sms: {}           {}by {}@tingirifistik\n  
     """.format(Fore.LIGHTCYAN_EX, len(servisler_sms), Style.RESET_ALL, Fore.LIGHTRED_EX))
